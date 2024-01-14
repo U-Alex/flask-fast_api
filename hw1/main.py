ac4731408c221d5d4ba771fdd3bd10bf58881533
UAA <uaa04@yandex.ru>
2024-01-14 14:14:23 +0900

    дз 1 семинар

--- hw1/main.py
+++ hw1/main.py
@@ -1,9 +1,31 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+cat_list = {'cat_list': ['одежда', 'обувь']}
+product_list = (['товар1', 'товар2', 'товар3', 'товар4'], ['товар5', 'товар6', 'товар7', 'товар8'])
 
+
+@app.route('/')
+def index():
+    context = cat_list
+    return render_template('category.html', context=context)
+
+
+@app.route('/<int:cat_num>/')
+def category(cat_num):
+    context = cat_list
+    context.update({'product_list': product_list[cat_num-1]})
+    return render_template('category.html', context=context)
+
+
+@app.route('/<int:cat_num>/<int:prod_id>/')
+def product(cat_num, prod_id):
+    context = cat_list
+    context.update({'product_list': product_list[cat_num-1]})
+    context.update({'product': product_list[cat_num-1][prod_id-1]})
+    return render_template('product.html', context=context)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
